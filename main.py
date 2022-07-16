d5446317f1bfaf22b4d365f8386bc64ebddf7a9a
edilaine10 <edilaine.duarte@yahoo.com>
2022-07-16 13:13:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 # View: o que vai para o usuário
-usuario_digitado = input("Informe o seu usuário: ")
-senha_digitada = input("Informe sua senha: ")
+def view():
+    usuario_digitado = input("Informe o seu usuário: ")
+    senha_digitada = input("Informe sua senha: ")
+    controller(usuario_digitado, senha_digitada)
 
 # Model: O que vem do banco de dados (BD)
-usuarioBD = 'joao'
-senhaBD = '123'
+def model_usuario():
+    usuarioBD = 'joao'
+    return usuarioBD
+
+def model_senha():
+    senhaBD = '123'
+    return senhaBD
 
 # Controller: a validação
-if usuario_digitado == usuarioBD and senha_digitada == senhaBD:
-    print("Pode entrar")
-else:
-    print("Usuário ou senha inválido")
+def controller(usuario_digitado, senha_digitada):
+    usuarioBD = model_usuario()
+    senhaBD = model_senha()
+    if usuario_digitado == usuarioBD and senha_digitada == senhaBD:
+        print("Pode entrar")
+    else:
+        print("Usuário ou senha inválido")
+
+
+view()
